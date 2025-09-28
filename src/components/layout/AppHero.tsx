961dba292185533ec41cd1cfaac91b32be9ee46c
Fran Aragón <franaragondeveloper@gmail.com>
2025-09-28 12:13:38 +0200

    fix video

--- src/components/layout/AppHero.tsx
+++ src/components/layout/AppHero.tsx
@@ -34,6 +34,7 @@ export default function AppHero() {
         poster="/hero-bg-frame.jpg"
         onLoadedData={() => setLoaded(true)}
       >
+        <source src="/hero-bg-720p.webm" type="video/webm" />
         <source src="/input.mp4" type="video/mp4" />
       </video>
 
